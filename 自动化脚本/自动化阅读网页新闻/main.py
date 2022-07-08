703e0f894517a79c576a80a20e7783b8db0c5226
shengdao <211471317@qq.com>
2022-07-08 10:01:17 +0800

    脚本编写

---
+++ 自动化脚本/自动化阅读网页新闻/main.py
@@ -0,0 +1,28 @@
+import pyttsx3
+import requests
+from bs4 import BeautifulSoup
+
+def speak(engine,audio):
+    engine.say(audio)
+    engine.runAndWait
+    return None
+
+def op():
+    engine=pyttsx3.init('sapi5')
+    voices=engine.getProperty('vocies')
+    newVoiceRate=130
+    engine.setProperty('rate',newVoiceRate)
+    text=str(input("Paste article\n"))
+    res=requests.get(text)
+    soup=BeautifulSoup(res.text,'html.parser')
+
+    articles=[]
+    for i in range(len(soup.select('.p'))):
+        article=soup.select('.p')[i].getText.strip()
+        articles.append(article)
+    text=" ".join(articles)
+    speak(engine,text)
+    return None
+
+if __name__=='__main__':
+    op()
